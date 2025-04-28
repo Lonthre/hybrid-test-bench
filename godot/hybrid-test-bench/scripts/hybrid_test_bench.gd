78b3dd2320687141d321a7ae9a17aa9cfc9d2e4c
Michal Kubirita <michal.kubirita@gmail.com>
2025-04-28 21:26:39 +0200

    improve Godot visualisation

--- godot/hybrid-test-bench/scripts/hybrid_test_bench.gd
+++ godot/hybrid-test-bench/scripts/hybrid_test_bench.gd
@@ -4,13 +4,18 @@ extends Node3D
 
 var shiny_steel = preload("res://materials/shiny_steel.tres")
 var base_colour = null
+@onready var indicator: CSGSphere3D = $CSGSphere3D
 
 
 func _ready() -> void:
 	base_colour = shiny_steel.albedo_color
 
-func move_actuator():
-	pass
+func set_actuator_on():
+	shiny_steel.albedo_color = Color.MEDIUM_SPRING_GREEN
 
-func change_actuator_colour():
-		shiny_steel.albedo_color = Color.LIGHT_CORAL
+func set_actuator_off():
+	shiny_steel.albedo_color = Color.INDIAN_RED
+
+func move_indicator(x, y):
+	indicator.position.x = x
+	indicator.position.y = y
--- godot/hybrid-test-bench/scripts/main.gd
+++ godot/hybrid-test-bench/scripts/main.gd
@@ -8,19 +8,22 @@ extends Node3D
 
 func _ready() -> void:
 	rabbit_mq.connect("OnMessage", _on_message)
-	#hybrid_test_bench.move_actuator()
 
 func _on_message(message):
 	var data = JSON.parse_string(message)
 	print(data)
 	
-	if not data.measurement == "emulator":
+	if data.measurement != "emulator":
 		return
-
-	if data.fields.force_on:
-		hybrid_test_bench.change_actuator_colour()
 	
+	if data.fields.force_on:
+		hybrid_test_bench.set_actuator_on()
+	else:
+		hybrid_test_bench.set_actuator_off()
+		
 	ui.update_vertical_force(data.fields.vertical_force)
 	ui.update_horizontal_force(data.fields.horizontal_force)
 	ui.update_vertical_displacement(data.fields.vertical_displacement)
 	ui.update_horizontal_displacement(data.fields.horizontal_displacement)
+
+	hybrid_test_bench.move_indicator(data.fields.horizontal_displacement, data.fields.vertical_displacement)
