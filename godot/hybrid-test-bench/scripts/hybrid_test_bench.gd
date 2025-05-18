7c7632b074f1356409a6d7c3df7b8704c8bc3fca
Michal Kubirita <michal.kubirita@gmail.com>
2025-05-18 13:52:06 +0200

    add visual displacement of the specimen in Godot

--- godot/hybrid-test-bench/scripts/hybrid_test_bench.gd
+++ godot/hybrid-test-bench/scripts/hybrid_test_bench.gd
@@ -4,13 +4,20 @@ extends Node3D
 
 var shiny_steel = preload("res://materials/shiny_steel.tres")
 var base_colour = null
-
+@onready var pivot: Marker3D = $Marker3D
+const SPECIMEN_LENGTH = 603
+var pivot_z_offset: float
 
 func _ready() -> void:
 	base_colour = shiny_steel.albedo_color
-
+	pivot_z_offset = pivot.position.z
+	
 func set_actuator_on():
 	shiny_steel.albedo_color = Color.MEDIUM_SPRING_GREEN
 
 func set_actuator_off():
 	shiny_steel.albedo_color = Color.INDIAN_RED
+
+func set_bending(vertical: float, horizontal:  float):
+	pivot.rotation_degrees.x = (rad_to_deg(tan(vertical / SPECIMEN_LENGTH)))	
+	pivot.position.z = horizontal / 1000 + pivot_z_offset
--- godot/hybrid-test-bench/scripts/main.gd
+++ godot/hybrid-test-bench/scripts/main.gd
@@ -9,6 +9,7 @@ extends Node3D
 func _ready() -> void:
 	rabbit_mq.connect("OnMessage", _on_message)
 
+
 func _on_message(message):
 	var data = JSON.parse_string(message)
 	print(data)
@@ -20,7 +21,9 @@ func _on_message(message):
 		hybrid_test_bench.set_actuator_on()
 	else:
 		hybrid_test_bench.set_actuator_off()
-		
+	
+	hybrid_test_bench.set_bending(data.fields.vertical_displacement, data.fields.horizontal_displacement)
+
 	ui.update_vertical_force(data.fields.vertical_force)
 	ui.update_horizontal_force(data.fields.horizontal_force)
 	ui.update_vertical_displacement(data.fields.vertical_displacement)
